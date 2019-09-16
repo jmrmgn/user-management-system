2b0108e15c6d14172673db7dacde5272e6c07117
Jomar <jomar.magno026@gmail.com>
2019-09-16 19:29:32 +0800

    setup of apollo client, WIP login and signup form

--- frontend/src/pages/Users/index.js
+++ frontend/src/pages/Users/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const usersQuery = gql`
+  query {
+    users {
+      _id
+      name
+      username
+    }
+  }
+`;
 
 function Users(props) {
+  const { loading, error, data } = useQuery(usersQuery);
+  if (loading) return <div>Loading...</div>;
+  if (error) return error.message;
+
+  console.log(data);
   return <span>Users...</span>;
 }
 
