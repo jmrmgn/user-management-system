41ab652e112da7a71bfb8334f52c930b1f8c4ac5
Jomar <jomar.magno026@gmail.com>
2019-09-16 19:40:56 +0800

    remove session related imports in server

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const session = require('express-session');
-const redis = require('redis');
-const RedisStore = require('connect-redis')(session);
 const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const schemaDirectives = require('./graphql/directives');
 const mongoose = require('mongoose');
 
-const TWO_HOURS = 1000 * 60 * 60 * 2;
-const {
-  PORT,
-  FRONTEND_URL,
-  NODE_ENV,
-  SESSION_NAME,
-  SESSION_SECRET,
-  DB_URI,
-  REDIS_HOST,
-  REDIS_PORT
-} = process.env;
+const { PORT, FRONTEND_URL, NODE_ENV, DB_URI } = process.env;
 const IN_PROD = NODE_ENV === 'production';
 
 (async () => {
@@ -27,29 +14,6 @@ const IN_PROD = NODE_ENV === 'production';
 
     const app = express();
     app.disable('x-powered-by');
-
-    const redisClient = redis.createClient();
-    const store = new RedisStore({
-      host: REDIS_HOST,
-      port: REDIS_PORT,
-      client: redisClient
-    });
-
-    app.use(
-      session({
-        store,
-        name: SESSION_NAME,
-        secret: SESSION_SECRET,
-        resave: true, // For refreshing of expiry of cookie base on the user activity
-        rolling: true,
-        saveUninitialized: false,
-        cookie: {
-          maxAge: parseInt(TWO_HOURS),
-          sameSite: true,
-          secure: IN_PROD
-        }
-      })
-    );
 
     const apolloServer = new ApolloServer({
       typeDefs,
